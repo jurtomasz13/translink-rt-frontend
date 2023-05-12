e74adba938ea8c71f46eeadc073a2995016c5976
Tomasz Jurek <jur.tomasz13@gmail.com>
2023-05-12 02:28:24 +0200

    fix: add components & services dependencies to appmodule, add basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from '../app/auth/auth.guard';
+import { TaskFormComponent } from './components/tasks/task-form/task-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks',
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', component: TasksComponent },
+      { path: 'create', component: TaskFormComponent },
+      { path: ':id/edit', component: TaskFormComponent },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  { path: '**', redirectTo: '/tasks' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskFormComponent } from './components/tasks/task-form/task-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TaskComponent } from './components/tasks/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskFormComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    TaskComponent,
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
